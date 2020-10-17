c2c9ce1462a0d51d5d54164e2894623576883943
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-10-17 19:47:54 +0200

    Add init structure, add test method for unit test.

---
+++ LISTED/Controller/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Test_LISTED
+{
+    public class Testing
+    {
+        public int variableTest = 0;
+        public int TestForTesting(int a, int b, int c)
+        {
+            c = a + b;
+            return c;
+        }
+    }
+
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GUI1());
+            
+            Testing test = new Testing();
+            test.variableTest = test.TestForTesting(1, 2, 0);
+        }
+    }
+
+
+}
---
+++ LISTED_UnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test_LISTED;
+
+
+namespace LISTED_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            int vara = 1;
+            int varb = 2;
+            int varc = 0;
+            Testing test = new Testing();
+
+            // act
+            test.variableTest = test.TestForTesting(vara, varb, varc);
+
+            // assert
+            Assert.AreEqual(3, test.variableTest, "Passed");
+
+        }
+    }
+}
