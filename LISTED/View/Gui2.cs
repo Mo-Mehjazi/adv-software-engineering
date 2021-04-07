21281d03e3cd9d416ba6d02139d1dcf2a54f2e63
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2021-04-07 17:12:25 +0200

    Fix missing data.

--- LISTED/View/Gui2.cs
+++ LISTED/View/Gui2.cs
@@ -15,5 +15,30 @@ namespace Test_LISTED.View
         {
             InitializeComponent();
         }
+
+        private void Gui2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
